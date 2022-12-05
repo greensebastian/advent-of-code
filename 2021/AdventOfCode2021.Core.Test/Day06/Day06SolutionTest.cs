c156a7ebc7787c1b5624c59058190127c331431e
Sebastian Green <xsebg@bankingcircle.com>
2022-12-05 15:32:00 +0100

    2021 day 6

---
+++ 2021/AdventOfCode2021.Core.Test/Day06/Day06SolutionTest.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode2021.Core.Day06;
+using Shouldly;
+
+namespace AdventOfCode2021.Core.Test.Day06;
+
+public class Day06SolutionTest
+{
+    [Fact]
+    public void FirstSolution_Example_Solves()
+    {
+        var input = Util.ReadFromFile("input");
+
+        var solution = new Day06Solution(input);
+
+        var actual = solution.FirstSolution().ToList();
+        
+        actual.Single().ShouldBe("5934");
+    }
+
+    [Fact]
+    public void FirstSolution_Real_Solves()
+    {
+        var input = Util.ReadFromFile("input");
+
+        var solution = new Day06Solution(input);
+
+        var actual = solution.FirstSolution().ToList();
+        
+        actual.Single().ShouldBe("373378");
+    }
+    
+    [Fact]
+    public void SecondSolution_Example_Solves()
+    {
+        var input = Util.ReadFromFile("input");
+
+        var solution = new Day06Solution(input);
+
+        var actual = solution.SecondSolution().ToList();
+        
+        actual.Single().ShouldBe("26984457539");
+    }
+
+    [Fact]
+    public void SecondSolution_Real_Solves()
+    {
+        var input = Util.ReadFromFile("input");
+
+        var solution = new Day06Solution(input);
+
+        var actual = solution.SecondSolution().ToList();
+        
+        actual.Single().ShouldBe("1682576647495");
+    }
+}
---
+++ 2021/AdventOfCode2021.Core/Day06/Day06Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode2021.Core.Day06;
+
+public record Day06Solution(IEnumerable<string> Input) : BaseSolution(Input)
+{
+    public override IEnumerable<string> FirstSolution()
+    {
+        var fishCountByDay = CountFish(80);
+
+        yield return fishCountByDay.Sum().ToString();
+    }
+    
+    public override IEnumerable<string> SecondSolution()
+    {
+        var fishCountByDay = CountFish(256);
+
+        yield return fishCountByDay.Sum().ToString();
+    }
+
+    private long[] CountFish(int daysToSimulate)
+    {
+        var fishCountByDay = new long[10];
+        foreach (var line in Input)
+        {
+            foreach (var days in line.Split(",").Select(byte.Parse))
+            {
+                fishCountByDay[days]++;
+            }
+
+            for (var day = 0; day < daysToSimulate; day++)
+            {
+                var fishToMultiply = fishCountByDay[0];
+                for (var daysLeft = 1; daysLeft < fishCountByDay.Length - 1; daysLeft++)
+                {
+                    fishCountByDay[daysLeft - 1] = fishCountByDay[daysLeft];
+                }
+
+                fishCountByDay[6] += fishToMultiply;
+                fishCountByDay[8] = fishToMultiply;
+
+                Console.WriteLine($"Count day {day}:\t{fishCountByDay.Sum()}");
+            }
+        }
+
+        return fishCountByDay;
+    }
+}
