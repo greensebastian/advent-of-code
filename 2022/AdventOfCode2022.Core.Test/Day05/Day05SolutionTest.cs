90654eee3af76aefe20a65ff590c2771b32cd203
Sebastian Green <sebastianpetergreen@outlook.com>
2022-12-05 07:32:45 +0100

    Day 5 2022

--- 2022/AdventOfCode2022.Core.Test/Day05/Day05SolutionTest.cs
+++ 2022/AdventOfCode2022.Core.Test/Day05/Day05SolutionTest.cs
@@ -14,7 +14,7 @@ public class Day05SolutionTest
 
         var actual = solution.FirstSolution().ToList();
         
-        actual.Single().ShouldBe("0");
+        actual.Single().ShouldBe("CMZ");
     }
 
     [Fact]
@@ -26,7 +26,7 @@ public class Day05SolutionTest
 
         var actual = solution.FirstSolution().ToList();
         
-        actual.Single().ShouldBe("0");
+        actual.Single().ShouldBe("RLFNRTNFB");
     }
     
     [Fact]
@@ -38,7 +38,7 @@ public class Day05SolutionTest
 
         var actual = solution.SecondSolution().ToList();
         
-        actual.Single().ShouldBe("0");
+        actual.Single().ShouldBe("MCD");
     }
 
     [Fact]
@@ -50,6 +50,6 @@ public class Day05SolutionTest
 
         var actual = solution.SecondSolution().ToList();
         
-        actual.Single().ShouldBe("0");
+        actual.Single().ShouldBe("MHQTLJRLB");
     }
 }
