78d5f2ad542430427271887705d5332046130584
Sebastian Green <sebastianpetergreen@outlook.com>
2022-12-06 08:29:58 +0100

    2022 Day 6

---
+++ 2022/AdventOfCode2022.Core.Test/Day06/Day06SolutionTest.cs
@@ -0,0 +1,65 @@
+﻿using AdventOfCode2022.Core.Day06;
+using Shouldly;
+
+namespace AdventOfCode2022.Core.Test.Day06;
+
+public class Day06SolutionTest
+{
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", "7")]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", "5")]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", "6")]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "10")]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "11")]
+    public void FirstSolution_Example_Solves(string input, string expectedCharactersBeforeMarker)
+    {
+        var inputLines = new[] { input };
+
+        var solution = new Day06Solution(inputLines);
+
+        var actual = solution.FirstSolution().ToList();
+        
+        actual.Single().ShouldBe(expectedCharactersBeforeMarker);
+    }
+
+    [Fact]
+    public void FirstSolution_Real_Solves()
+    {
+        var input = Util.ReadFromFile("input");
+
+        var solution = new Day06Solution(input);
+
+        var actual = solution.FirstSolution().ToList();
+        
+        actual.Single().ShouldBe("1892");
+    }
+    
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", "19")]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", "23")]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", "23")]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "29")]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "26")]
+    public void SecondSolution_Example_Solves(string input, string expectedCharactersBeforeMarker)
+    {
+        var inputLines = new[] { input };
+
+        var solution = new Day06Solution(inputLines);
+
+        var actual = solution.SecondSolution().ToList();
+        
+        actual.Single().ShouldBe(expectedCharactersBeforeMarker);
+    }
+
+    [Fact]
+    public void SecondSolution_Real_Solves()
+    {
+        var input = Util.ReadFromFile("input");
+
+        var solution = new Day06Solution(input);
+
+        var actual = solution.SecondSolution().ToList();
+        
+        actual.Single().ShouldBe("2313");
+    }
+}
---
+++ 2022/AdventOfCode2022.Core/Day06/Day06Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2022.Core.Day06;
+
+public record Day06Solution(IEnumerable<string> Input) : BaseSolution(Input)
+{
+    public override IEnumerable<string> FirstSolution()
+    {
+        // Find 4 different characters in a row
+        foreach (var line in Input)
+        {
+            var processed = GetCountBeforeUniqueSequence(line, 4);
+
+            yield return processed.ToString();
+        }
+    }
+
+    public override IEnumerable<string> SecondSolution()
+    {
+        // Find 4 different characters in a row
+        foreach (var line in Input)
+        {
+            var processed = GetCountBeforeUniqueSequence(line, 14);
+
+            yield return processed.ToString();
+        }
+    }
+    
+    private static int GetCountBeforeUniqueSequence(string line, int requiredUnique)
+    {
+        var processed = 0;
+        var foundSequence = false;
+        var data = new LinkedList<char>(line);
+        var nextNode = data.First;
+        while (nextNode is not null && !foundSequence)
+        {
+            processed++;
+            var current = nextNode;
+            nextNode = current.Next;
+            var encountered = new HashSet<char> { current.Value };
+            // Walk backwards to check uniqueness
+            for (var i = 1; i <= requiredUnique; i++)
+            {
+                current = current.Previous;
+                if (current is null) break;
+                if (!encountered.Add(current.Value)) break;
+            }
+
+            if (encountered.Count == requiredUnique)
+                foundSequence = true;
+        }
+
+        return processed;
+    }
+}
