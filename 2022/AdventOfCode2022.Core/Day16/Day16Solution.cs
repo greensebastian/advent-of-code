89b9625f9700e9f3a57a6f4742707ffabc58b653
Sebastian Green <sgre@netlight.com>
2022-12-16 09:02:36 +0100

    Slow

--- 2022/AdventOfCode2022.Core/Day16/Day16Solution.cs
+++ 2022/AdventOfCode2022.Core/Day16/Day16Solution.cs
@@ -8,12 +8,95 @@ public record Day16Solution(IEnumerable<string> Input) : BaseSolution(Input)
 
         var connections = ValveDijkstra.GetQuickestConnections(valveSystem).ToList();
 
-        yield return "0";
+        var paths = new List<ValveSystemPath>();
+        var done = new List<ValveSystemPath>();
+        paths.Add(ValveSystemPath.New);
+        paths.Add(ValveSystemPath.New with { Opened = "AA", TimeElapsed = 1 });
+        while (paths.Count > 0)
+        {
+            Console.WriteLine($"PathCount: {paths.Count}, FirstTimeElapsed: {paths[0].TimeElapsed}");
+            var newPaths = new List<ValveSystemPath>();
+            foreach (var path in paths)
+            {
+                if (path.TimeElapsed >= 30)
+                {
+                    done.Add(path);
+                    continue;
+                }
+
+                var scoreDelta = path.GetScoreForOneMinute(valveSystem);
+                var valvesToCheck = connections.Where(c => c.Start == path.Current);
+                foreach (var nextValve in connections.Where(c => c.Start == path.Current))
+                {
+                    if (path.Opened.Contains(nextValve.End)) continue;
+                    AddOrReplaceEquivalent(path with
+                    {
+                        Current = nextValve.End,
+                        Score = path.Score + scoreDelta,
+                        TimeElapsed = path.TimeElapsed + 1
+                    }, newPaths);
+                    if (path.TimeElapsed < 29)
+                    {
+                        AddOrReplaceEquivalent(path with
+                        {
+                            Current = nextValve.End,
+                            Score = path.Score + 2 * scoreDelta,
+                            TimeElapsed = path.TimeElapsed + 2,
+                            Opened = path.Opened + nextValve.End
+                        }, newPaths);
+                    }
+                }
+            }
+
+            paths = newPaths;
+        }
+
+        yield return done.Max(p => p.Score).ToString();
+    }
+
+    private static void AddOrReplaceEquivalent(ValveSystemPath toAdd, List<ValveSystemPath> allExisting)
+    {
+        var exists = allExisting.Any(e => e.Equivalent(toAdd));
+        if (!exists) allExisting.Add(toAdd);
+        else
+        {
+            var existing = allExisting.Single(e => e.Equivalent(toAdd));
+            if (existing.Score >= toAdd.Score) return;
+
+            allExisting.Remove(existing);
+            allExisting.Add(toAdd);
+        }
     }
     
     public override IEnumerable<string> SecondSolution(params string[] args)
     {
         yield return "0";
+    }
+}
+
+public record struct ValveSystemPath(int TimeElapsed, string Opened, string Current, int Score)
+{
+    public static ValveSystemPath New => new(0, string.Empty, "AA", 0);
+    
+    public int GetScoreForOneMinute(ValveSystem system)
+    {
+        var roundPoints = 0;
+        foreach (var open in Opened.Chunk(2).Select(c => $"{c[0]}{c[1]}"))
+        {
+            roundPoints += system.AllValves[open].FlowRate;
+        }
+
+        return roundPoints;
+    }
+
+    public bool Equivalent(ValveSystemPath other)
+    {
+        if (Current != other.Current) return false;
+        if (TimeElapsed != other.TimeElapsed) return false;
+        if (Opened.Length != other.Opened.Length) return false;
+        var thisSet = string.Join("", Opened.Chunk(2).Select(c => $"{c[0]}{c[1]}").Order());
+        var otherSet = string.Join("", other.Opened.Chunk(2).Select(c => $"{c[0]}{c[1]}").Order());
+        return thisSet == otherSet;
     }
 }
 
