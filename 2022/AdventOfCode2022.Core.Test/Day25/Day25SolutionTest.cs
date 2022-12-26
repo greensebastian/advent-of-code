f6a81c038b0f68aae98e49bf9de4a99a89d237f6
Sebastian Green <sgre@netlight.com>
2022-12-26 22:31:26 +0100

    2022 Day 25 WIP

--- 2022/AdventOfCode2022.Core.Test/Day25/Day25SolutionTest.cs
+++ 2022/AdventOfCode2022.Core.Test/Day25/Day25SolutionTest.cs
@@ -5,6 +5,19 @@ namespace AdventOfCode2022.Core.Test.Day25;
 
 public class Day25SolutionTest
 {
+    [Theory]
+    [InlineData(10, "20")]
+    [InlineData(11, "21")]
+    [InlineData(12, "22")]
+    [InlineData(13, "1==")]
+    [InlineData(-12, "==")]
+    [InlineData(-13, "-22")]
+    public void Convert_To_Snafu(long value, string expectedSnafu)
+    {
+        var actual = SnafuConverter.Convert(value);
+        actual.ShouldBe(expectedSnafu);
+    }
+    
     [Fact]
     public void FirstSolution_Example_Solves()
     {
@@ -14,19 +27,21 @@ public class Day25SolutionTest
 
         var actual = solution.FirstSolution().ToList();
 
-        actual.Single().ShouldBe("0");
+        actual.Single().ShouldBe("2=-1=0");
     }
 
     [Fact]
-    public void FirstSolution_Real_Solves()
+    public async Task FirstSolution_Real_Solves()
     {
-        var input = Util.ReadFromFile("input");
+        var input = await Util.ReadFromCachedFile("day25");
 
         var solution = new Day25Solution(input, _ => {});
 
         var actual = solution.FirstSolution().ToList();
 
         actual.Single().ShouldBe("0");
+        // 27766274000 wrong
+        // 6291437520 Wrong
     }
 
     [Fact]
--- 2022/AdventOfCode2022.Core/Day25/Day25Solution.cs
+++ 2022/AdventOfCode2022.Core/Day25/Day25Solution.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
 
 namespace AdventOfCode2022.Core.Day25;
 
@@ -6,16 +7,60 @@ public record Day25Solution(IEnumerable<string> Input, Action<string> Log) : Bas
 {
     public override IEnumerable<string> FirstSolution(params string[] args)
     {
+        var sum = 0L;
         foreach (var line in Input)
         {
-            var nbr = long.Parse(line, CultureInfo.InvariantCulture);
+            sum += SnafuConverter.Convert(line);
         }
 
-        yield return "0";
+        yield return sum.ToString();
     }
     
     public override IEnumerable<string> SecondSolution(params string[] args)
     {
         yield return "0";
+    }
+}
+
+public static class SnafuConverter
+{
+    private static IReadOnlyDictionary<char, int> Snafu2Decimal { get; } = new Dictionary<char, int>
+    {
+        { '2', 2 },
+        { '1', 1 },
+        { '0', 0 },
+        { '-', -1 },
+        { '=', -2 }
+    };
+
+    public static long Convert(string snafu)
+    {
+        var m = 1;
+        var sum = 0L;
+        foreach (var c in snafu.Reverse())
+        {
+            sum += Snafu2Decimal[c] * m;
+            m *= 5;
+        }
+
+        return sum;
+    }
+
+    public static string Convert(long value)
+    {
+        var abs = Math.Abs(value);
+        var maxMultiple = 1;
+        var reachable = 2;
+        while (reachable < abs)
+        {
+            maxMultiple *= 5;
+            reachable += 2 * maxMultiple;
+        }
+
+        var remainder = value;
+        var nextMultiple = maxMultiple / 5;
+        var nextReachable = reachable - 1 - 2 * nextMultiple;
+
+        return "";
     }
 }
