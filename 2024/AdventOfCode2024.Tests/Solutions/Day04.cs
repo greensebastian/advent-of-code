b8bd0bfd0f24482c759b3ea1fffb79a071ee6609
Sebastian Green <sgre@netlight.com>
2024-12-14 16:28:38 +0100

    2024 04 2 done

---
+++ 2024/AdventOfCode2024.Tests/Solutions/Day04.cs
@@ -0,0 +1,107 @@
+﻿using System.Text.RegularExpressions;
+using FluentAssertions;
+
+namespace AdventOfCode2024.Tests.Solutions;
+
+public class Day04 : ISolution
+{
+    private const string Example = """
+                                   MMMSXXMASM
+                                   MSAMXMSMSA
+                                   AMXSXMAAMM
+                                   MSAMASMSMX
+                                   XMASAMXAMM
+                                   XXAMMXXAMA
+                                   SMSMSASXSS
+                                   SAXAMASAAA
+                                   MAMMMXMMMM
+                                   MXMXAXMASX
+                                   """;
+    
+    [Fact]
+    public void Solution1()
+    {
+        //var input = Util.ReadRaw(Example);
+        var input = Util.ReadFile("day04");
+
+        var letters = new Dictionary<Point, char>();
+        for (var row = 0; row < input.Length; row++)
+        {
+            for (var col = 0; col < input[row].Length; col++)
+            {
+                letters[new Point(row, col)] = input[row][col];
+            }
+        }
+
+        var answer = XmasLine.ValidXmas(letters);
+        answer.Should().Be(2468);
+    }
+
+    private record XmasLine(Point Start, Point End)
+    {
+        public static int ValidXmas(IReadOnlyDictionary<Point, char> letters)
+        {
+            var count = 0;
+            foreach (var (xStart, _) in letters.Where(l => l.Value == 'X'))
+            {
+                foreach (var neighbour in xStart.ClockwiseNeighboursWithDiagonal)
+                {
+                    if (IsValidXmas(xStart, neighbour - xStart)) count++;
+                }
+            }
+
+            return count;
+
+            bool IsValidXmas(Point initial, Point dir)
+            {
+                var p = initial;
+                foreach (var character in "XMAS")
+                {
+                    if (!letters.TryGetValue(p, out var c) || c != character) return false;
+                    p += dir;
+                }
+
+                return true;
+            }
+        }
+        
+        public static int ValidXDashMas(IReadOnlyDictionary<Point, char> letters)
+        {
+            var count = 0;
+            foreach (var (mid, _) in letters.Where(l => l.Value == 'A'))
+            {
+                var tlbr = IsValidXDashMas(mid, mid.Up.Left - mid);
+                var trbl = IsValidXDashMas(mid, mid.Up.Right - mid);
+                if (tlbr && trbl) count++;
+            }
+
+            return count;
+            
+            bool IsValidXDashMas(Point initial, Point dir)
+            {
+                if (!letters.ContainsKey(initial + dir) || !letters.ContainsKey(initial - dir)) return false;
+                return (letters[initial + dir] == 'S' && letters[initial - dir] == 'M') ||
+                       (letters[initial + dir] == 'M' && letters[initial - dir] == 'S');
+            }
+        }
+    }
+    
+    [Fact]
+    public void Solution2()
+    {
+        //var input = Util.ReadRaw(Example);
+        var input = Util.ReadFile("day04");
+
+        var letters = new Dictionary<Point, char>();
+        for (var row = 0; row < input.Length; row++)
+        {
+            for (var col = 0; col < input[row].Length; col++)
+            {
+                letters[new Point(row, col)] = input[row][col];
+            }
+        }
+
+        var answer = XmasLine.ValidXDashMas(letters);
+        answer.Should().Be(1864);
+    }
+}
--- 2024/AdventOfCode2024.Tests/Util.cs
+++ 2024/AdventOfCode2024.Tests/Util.cs
@@ -18,3 +18,34 @@ public static class Util
 
     public static string[] ReadRaw(string lines) => CleanInput(lines.Split("\n"));
 }
+
+public readonly record struct Point(int Row, int Col)
+{
+    public Point Up => this with { Row = Row - 1 };
+    public Point Right => this with { Col = Col + 1 };
+    public Point Down => this with { Row = Row + 1 };
+    public Point Left => this with { Col = Col - 1 };
+
+    public IEnumerable<Point> ClockwiseNeighboursWithDiagonal =>
+        [Up, Up.Right, Right, Right.Down, Down, Down.Left, Left, Left.Up];
+
+    public static Point operator +(Point a, Point b) => new(Row: a.Row + b.Row, Col: a.Col + b.Col);
+    public static Point operator -(Point a, Point b) => new(Row: a.Row - b.Row, Col: a.Col - b.Col);
+    
+    public Point DirTo(Point other)
+    {
+        var diff = other - this;
+        if (diff.Row == 0) return new Point(0, Math.Clamp(diff.Col, -1, 1));
+        if (diff.Col == 0) return new Point(Math.Clamp(diff.Row, -1, 1), 0);
+        if (Math.Abs(diff.Row) != Math.Abs(diff.Col)) throw new ArgumentException($"Difference not orthogonal or diagonal, {diff}", nameof(other));
+        return (diff.Col > 0, diff.Row > 0) switch
+        {
+            (true, true) => new Point(1, 1),
+            (true, false) => new Point(1, -1),
+            (false, true) => new Point(-1, 1),
+            (false, false) => new Point(-1, -1)
+        };
+    }
+
+    public override string ToString() => $"[{Row}, {Col}]";
+}
