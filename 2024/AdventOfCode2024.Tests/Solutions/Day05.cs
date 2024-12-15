e704956536d4e3c06f87276fce52da1f114babb0
Sebastian Green <sgre@netlight.com>
2024-12-15 10:27:36 +0100

    2024 05 2

--- 2024/AdventOfCode2024.Tests/Solutions/Day05.cs
+++ 2024/AdventOfCode2024.Tests/Solutions/Day05.cs
@@ -50,9 +50,10 @@ public class Day05 : ISolution
     public void Solution2()
     {
         //var input = Util.ReadRaw(Example);
-        var input = Util.ReadFile("day04");
+        var input = Util.ReadFile("day05");
 
-        
+        var answer = PrintingDirectives.CorrectPrintingCorrectedMediansSum(input);
+        answer.Should().Be(4260);
     }
 
     private record PrintingDirectives
@@ -80,6 +81,45 @@ public class Day05 : ISolution
                 return true;
             });
             return valid.Sum(v => v[v.Length / 2]);
+        }
+        
+        public static int CorrectPrintingCorrectedMediansSum(string[] lines)
+        {
+            var rules = lines.TakeWhile(l => !string.IsNullOrWhiteSpace(l)).Select(l => new { Prerequisite = int.Parse(l[..2]), Page = int.Parse(l[3..]) }).ToArray();
+            var requirements = rules.SelectMany(r => new[] { r.Prerequisite, r.Page }).Distinct()
+                .ToDictionary(r => r, _ => new HashSet<int>());
+            foreach (var rule in rules)
+            {
+                requirements[rule.Page].Add(rule.Prerequisite);
+            }
+            var updates = lines.Skip(rules.Length + 1).Select(l => l.Split(',').Select(int.Parse).ToArray()).ToArray();
+            var invalid = updates.Where(update =>
+            {
+                var seen = new HashSet<int>();
+                foreach (var page in update)
+                {
+                    if (!requirements.ContainsKey(page)) requirements[page] = new HashSet<int>();
+                    if (requirements[page].Any(req => update.Contains(req) && !seen.Contains(req))) return true;
+                    seen.Add(page);
+                }
+
+                return false;
+            });
+
+            var valids = new List<int[]>();
+            foreach (var update in invalid)
+            {
+                var valid = new int[update.Length];
+                for (var i = 0; i < valid.Length; i++)
+                {
+                    var toUse = update.Where(n => !valid.Contains(n)).First(n =>
+                        !requirements[n].Any(r => update.Contains(r) && !valid[..i].Contains(r)));
+                    valid[i] = toUse;
+                }
+                valids.Add(valid);
+            }
+            
+            return valids.Sum(v => v[v.Length / 2]);
         }
     }
 }
