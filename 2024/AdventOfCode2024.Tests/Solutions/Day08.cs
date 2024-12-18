2ac205d6536ae32bfcfcc1ede29e7d12815ecf71
Sebastian Green <sebastianpetergreen@outlook.com>
2024-12-18 10:36:48 +0100

    2024 08 2

--- 2024/AdventOfCode2024.Tests/Solutions/Day08.cs
+++ 2024/AdventOfCode2024.Tests/Solutions/Day08.cs
@@ -26,7 +26,7 @@ public class Day08 : ISolution
         var input = Util.ReadFile("day08");
         
         var answer = UniqueAntiNodeLocations(input);
-        answer.Should().Be(14);
+        answer.Should().Be(254);
     }
 
     [Fact]
@@ -35,8 +35,8 @@ public class Day08 : ISolution
         //var input = Util.ReadRaw(Example);
         var input = Util.ReadFile("day08");
 
-        var answer = UniqueAntiNodeLocations(input);
-        answer.Should().Be(9214);
+        var answer = UniqueRepeatingAntiNodeLocations(input);
+        answer.Should().Be(951);
     }
 
     private int UniqueAntiNodeLocations(string[] lines)
@@ -54,6 +54,39 @@ public class Day08 : ISolution
                 var a2Ext = a2 + (a2 - a1);
                 if (map.ContainsKey(a1Ext)) nodeLocations.Add(a1Ext);
                 if (map.ContainsKey(a2Ext)) nodeLocations.Add(a2Ext);
+            }
+        }
+
+        return nodeLocations.Count;
+    }
+    
+    private int UniqueRepeatingAntiNodeLocations(string[] lines)
+    {
+        var nodeLocations = new HashSet<Point>();
+        var map = Point.GetMap(lines, c => c);
+
+        foreach (var antennaGrouping in map.Where(kv => kv.Value != '.').GroupBy(kv => kv.Value))
+        {
+            foreach (var antennaPair in antennaGrouping.Combinations().Where(c => c[0].Key != c[1].Key))
+            {
+                var a1 = antennaPair[0].Key;
+                var a2 = antennaPair[1].Key;
+
+                var p = a1;
+                var dir = a1 - a2;
+                while (map.ContainsKey(p))
+                {
+                    nodeLocations.Add(p);
+                    p += dir;
+                }
+                
+                p = a2;
+                dir = a2 - a1;
+                while (map.ContainsKey(p))
+                {
+                    nodeLocations.Add(p);
+                    p += dir;
+                }
             }
         }
 
