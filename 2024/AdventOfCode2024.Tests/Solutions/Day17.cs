ac888e2eb9b865a1745e956e474b15fe92c5cbeb
Sebastian Green <sebastianpetergreen@outlook.com>
2024-12-28 22:27:22 +0100

    2024 17 2

--- 2024/AdventOfCode2024.Tests/Solutions/Day17.cs
+++ 2024/AdventOfCode2024.Tests/Solutions/Day17.cs
@@ -36,8 +36,24 @@ public class Day17 : ISolution
         //var input = Util.ReadRaw(ExampleTwo);
         var input = Util.ReadFile("day17");
 
-        var result = BruteForceInput(input);
-        result.Should().Be(117440);
+        var res = FindAOptionRecursive(Seq);
+        res.Should().Be(164278496489149L);
+    }
+    
+    [Fact]
+    public void TestStuff()
+    {
+        foreach (var option in FindAOptions([5,4,2,5,5,0,3,3,0]))
+        {
+            Console.WriteLine($"{option:B48}");
+        }
+    }
+    
+    [Fact]
+    public void TestRecursion()
+    {
+        var res = FindAOptionRecursive(Seq);
+        Console.WriteLine($"{res:B48}");
     }
 
     private string SolveProgram(string[] input)
@@ -47,18 +63,101 @@ public class Day17 : ISolution
     
     private long BruteForceInput(string[] input)
     {
-        for (var a = 1L; a < 1_000_000; a++)
+        var baseMachine = Machine.FromInput(input);
+        var targetOutput = string.Join(",", baseMachine.Sequence);
+        for (var a = 1L; a < 10_000_000_000; a++)
         {
-            var machine = Machine.FromInput(input);
-            var output = Machine.FromInput(input).Run(output =>
-            {
-                if (output.Length > machine.Sequence.Length) return true;
-                return output.Where((t, i) => t != machine.Sequence[i]).Any();
-            }, a);
-            if (output == string.Join(",", machine.Sequence)) return a;
+            var output = new Machine(a, 0, 0, baseMachine.Sequence).Run(ImpossibleOutput, a);
+            if (output == targetOutput) return a;
         }
 
         return -1;
+
+        bool ImpossibleOutput(ICollection<long> output)
+        {
+            if (baseMachine == null) throw new Exception();
+            
+            if (output.Count > baseMachine.Sequence.Length) return true;
+            return output.Where((t, i) => t != baseMachine.Sequence[i]).Any();
+        }
+    }
+
+    private static readonly byte[] Seq = { 2, 4, 1, 1, 7, 5, 1, 5, 4, 2, 5, 5, 0, 3, 3, 0 };
+    
+    private static bool MatchesOutput(ICollection<byte> output, byte[] seq)
+    {
+        if (output.Count != seq.Length) return false;
+        return !output.Where((t, i) => t != seq[i]).Any();
+    }
+    
+    private static long FindA()
+    {
+        for (var startA = 1L << Seq.Length * 3 - 1; startA < 1L << (Seq.Length * 3 + 3); startA++)
+        {
+            if (PrintsItself(startA, Seq)) return startA;
+        }
+
+        return -1;
+    }
+    
+    private static IEnumerable<long> FindAOptions(byte[] seq)
+    {
+        for (var startA = 1L << (seq.Length - 1) * 3; startA < 1L << seq.Length * 3; startA++)
+        //for (var startA = 1L; startA < 1_000_000_000; startA++)
+        {
+            if (PrintsItself(startA, seq)) yield return startA;
+        }
+    }
+    
+    private static long FindAOptionRecursive(byte[] seq)
+    {
+        if (seq.Length == 1)
+        {
+            for (var startA = 1L; startA < 8; startA++)
+            {
+                if (PrintsItself(startA, seq)) return startA;
+            }
+
+            throw new Exception();
+        }
+
+        var min = FindAOptionRecursive(seq[1..]) << 3;
+        for (var startA = min; startA < min << 3; startA++)
+        {
+            if (PrintsItself(startA, seq)) return startA;
+        }
+
+        throw new Exception();
+    }
+    
+    private static bool PrintsItself(long a, byte[] seq)
+    {
+        var output = new List<byte>(seq.Length);
+        while (a != 0 && output.Count < seq.Length)
+        {
+            // ((((a & 0b111) ^ 1) ^ 5) ^ (a >> ((a & 0b111) ^ 1))) & 0b111
+            // 
+            // 2,4
+            var b = a & 0b111;
+            // 1,1
+            b ^= 1;
+            // 7,5
+            var c = a >> (int)b;
+            // 1,5
+            b ^= 5;
+            // 4,2
+            b ^= c;
+            // 5,5
+            var needed = seq[output.Count];
+            var toOutput = (byte)(b & 0b111);
+            if (toOutput != needed) break;
+            output.Add(toOutput);
+            // 0,3
+            a >>= 3;
+            // 3,0
+        }
+
+        return MatchesOutput(output, seq);
     }
 
     private class Machine(long a, long b, long c, long[] sequence)
@@ -71,30 +170,29 @@ public class Day17 : ISolution
                 input[2].PlusMinusLongs().Single(), input[4].PlusMinusLongs().ToArray());
         }
         
-        public string Run(Func<long[], bool> exitCondition, long? aReplacement = null)
+        public string Run(Func<IList<long>, bool> impossibleOutput, long? aReplacement = null)
         {
             if (aReplacement is not null) a = aReplacement.Value;
             var output = new List<long>();
             for (var pointer = 0L; pointer < sequence.Length; pointer += 2)
             {
-                if (exitCondition(output.ToArray())) return "";
                 var opcode = sequence[pointer];
                 var operand = sequence[pointer + 1];
                 switch (opcode)
                 {
                     case 0:
                     {
-                        a = (long)(a / Math.Pow(2, ComboOperand(operand)));
+                        a >>= (int)ComboOperand(operand);
                         break;
                     }
                     case 1:
                     {
-                        b = b ^ operand;
+                        b ^= operand;
                         break;
                     }
                     case 2:
                     {
-                        b = ComboOperand(operand) % 8;
+                        b = ComboOperand(operand) & 0b111;
                         break;
                     }
                     case 3 when a != 0:
@@ -109,17 +207,18 @@ public class Day17 : ISolution
                     }
                     case 5:
                     {
-                        output.Add(ComboOperand(operand) % 8);
+                        output.Add(ComboOperand(operand) & 0b111);
+                        if (impossibleOutput(output)) return "";
                         break;
                     }
                     case 6:
                     {
-                        b = (long)(a / Math.Pow(2, ComboOperand(operand)));
+                        b = a >> (int)ComboOperand(operand);
                         break;
                     }
                     case 7:
                     {
-                        c = (long)(a / Math.Pow(2, ComboOperand(operand)));
+                        c = a >> (int)ComboOperand(operand);
                         break;
                     }
                 }
