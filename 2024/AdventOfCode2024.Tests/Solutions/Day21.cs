c31c6a2d8ae6a40a73c3753961677002bb46b149
Sebastian Green <sebastianpetergreen@outlook.com>
2025-01-03 13:52:51 +0100

    2024 21 1

--- 2024/AdventOfCode2024.Tests/Solutions/Day21.cs
+++ 2024/AdventOfCode2024.Tests/Solutions/Day21.cs
@@ -24,14 +24,18 @@ public class Day21 : ISolution
     [Fact]
     public void Solution1()
     {
-        var input = Util.ReadRaw(ShortExample);
+        //var input = Util.ReadRaw(ShortExample);
         //var input = Util.ReadRaw(Example);
         //var input = Util.ReadRaw(SecondExample);
-        //var input = Util.ReadFile("day20");
+        var input = Util.ReadFile("day21");
 
         var r = new RobotArrangement(input, [Directional, Directional, Directional, Numeric]);
         var sum = r.CountScore();
-        sum.Should().Be(64 * 379);
+        sum.Should().Be(177814);
+        
+        // 184390L too high
+        // 184058L too high
+        // 184058L
     }
 
     [Fact]
@@ -64,32 +68,116 @@ public class Day21 : ISolution
             var score = 0L;
             foreach (var target in Targets)
             {
-                var shortestSequence = ShortestSequence(target);
+                var shortestSequence = ShortestSequenceDijkstra(target);
                 var numeric = target.PlusMinusLongs().Single();
+                Console.WriteLine($"{target}: {shortestSequence.Length} * {numeric}");
                 score += shortestSequence.Length * numeric;
             }
 
             return score;
         }
 
+        private record MovePointer(string Moves, Pointer Pointer);
+
+        private record DijkstraState(int Level, string Moves, string Remainder, Pointer LowerLevelPointer)
+        {
+            public override string ToString() => $"{Level}, [{Moves} - {Remainder}]";
+        }
+        
+        private string ShortestSequenceDijkstra(string target)
+        {
+            var best = new DijkstraState(levels.Length - 1, "", target,
+                new Pointer(levels.Last().Single(kv => kv.Value == 'A').Key, levels.Last()));
+            var root = new Node<DijkstraState>(best, null);
+            var responses = root.Dijkstra(0, node =>
+            {
+                if (node.Value.Remainder == "")
+                {
+                    var nextLevelIndex = node.Value.Level - 1;
+                    return
+                    [
+                        new DijkstraState(nextLevelIndex, "", node.Value.Moves,
+                            new Pointer(levels[nextLevelIndex].Single(kv => kv.Value == 'A').Key,
+                                levels[nextLevelIndex]))
+                    ];
+                }
+
+                var primary = node.Value.LowerLevelPointer.NeededToPress(node.Value.Remainder[0], true);
+                var alternate = node.Value.LowerLevelPointer.NeededToPress(node.Value.Remainder[0], false);
+                return
+                [
+                    node.Value with
+                    {
+                        Moves = node.Value.Moves + primary.moves,
+                        Remainder = node.Value.Remainder[1..],
+                        LowerLevelPointer = primary.newPointer
+                    },
+                    node.Value with
+                    {
+                        Moves = node.Value.Moves + alternate.moves,
+                        Remainder = node.Value.Remainder[1..],
+                        LowerLevelPointer = alternate.newPointer
+                    }
+                ];
+            }, node =>
+            {
+                if (node.Value.Moves.Length == 0) return 0;
+                return node.Value.Moves.Length - node.Previous!.Value.Moves.Length;
+            }, node => node.Value is { Level: 1, Remainder.Length: 0 }, (node, dist) =>
+            {
+                if (node.Value.Level != 0) return false;
+
+                if (best.Level > 0)
+                {
+                    best = node.Value;
+                    return false;
+                }
+
+                var remainderDiff = best.Remainder.Length - node.Value.Remainder.Length;
+                var movesDiff = node.Value.Moves.Length - best.Moves.Length;
+                var botInSameSpot = node.Value.LowerLevelPointer.Position == best.LowerLevelPointer.Position;
+                if (remainderDiff > 0)
+                {
+                    best = node.Value;
+                    return false;
+                }
+
+                if (movesDiff == 0 && remainderDiff == 0 && botInSameSpot) return true;
+                if (node.Value.Moves.Length > 0 && best.Moves.Length > 0 && movesDiff > 0) return true;
+                return false;
+            });
+
+            return responses.First().EndNode.Value.Moves;
+        }
+
         private string ShortestSequence(string target)
         {
-            var moves = target;
+            var moveOptions = new List<MovePointer> {new(target, new Pointer(levels.Last().Single(kv => kv.Value == 'A').Key, levels.Last()))};
             for (var i = levels.Length - 1; i > 0; i--)
             {
-                var lowerLevel = levels[i];
-                var lowerPointer = new Pointer(lowerLevel.Single(kv => kv.Value == 'A').Key, lowerLevel);
-                var newMoves = "";
-                foreach (var move in moves)
+                var newMoveOptions = new List<MovePointer>();
+                foreach (var moves in moveOptions)
                 {
-                    var (upperMoves, newLowerPointer) = lowerPointer.NeededToPress(move, ' ');
-                    newMoves += upperMoves;
-                    lowerPointer = newLowerPointer;
+                    var lowerLevel = levels[i];
+                    var lowerPointer = moves.Pointer with { KeyPad = lowerLevel };
+                    var newMoves = "";
+                    foreach (var move in moves.Moves)
+                    {
+                        var primary = lowerPointer.NeededToPress(move, true);
+                        var alternate = lowerPointer.NeededToPress(move, false);
+                        var (upperMoves, newLowerPointer) =
+                            alternate.moves.Length < primary.moves.Length ? alternate : primary;
+                        newMoves += upperMoves;
+                        lowerPointer = newLowerPointer;
+                    }
+                    Console.WriteLine($"Level {i}: {moves} => {newMoves}");
+                    newMoveOptions.Add(new MovePointer(newMoves, lowerPointer));
                 }
-                moves = newMoves;
+
+                moveOptions = newMoveOptions;
             }
 
-            return moves;
+            return moveOptions.MinBy(s => s.Moves.Length)!.Moves;
         }
     }
 
@@ -101,7 +189,7 @@ public class Day21 : ISolution
     {
         private readonly Point _void = KeyPad.Single(kv => kv.Value == '.').Key;
         
-        public (string moves, Pointer newPointer) NeededToPress(char end, char upperChar)
+        public (string moves, Pointer newPointer) NeededToPress(char end, bool prioritizeVertical)
         {
             var endPoint = KeyPad.Single(kv => kv.Value == end).Key;
             var move = endPoint - Position;
@@ -109,10 +197,12 @@ public class Day21 : ISolution
             var horizontals = Math.Abs(move.Col);
             var verticalPart = string.Concat(Enumerable.Repeat(move.Row > 0 ? 'v' : '^', (int)verticals));
             var horizontalPart = string.Concat(Enumerable.Repeat(move.Col > 0 ? '>' : '<', (int)horizontals));
-            var canDoVerticalFirst = Position.Col != _void.Col;
-            var shouldDoVerticalFirst = canDoVerticalFirst && false;
-            var moves = shouldDoVerticalFirst ? verticalPart + horizontalPart : horizontalPart + verticalPart;
-            return (moves + "A", this with { Position = endPoint });
+            var hasToDoVerticalFirst = (endPoint.Col == _void.Col && Position.Row == _void.Row) || (endPoint.Row == _void.Row && Position.Col == _void.Col);
+            var hasToDoHorizontalFirst = (endPoint.Row == _void.Row && Position.Col == _void.Col) || (endPoint.Col == _void.Col && Position.Row == _void.Row);
+            var shouldDoVerticalFirst = hasToDoVerticalFirst ? true : hasToDoHorizontalFirst ? false : prioritizeVertical;
+            var moves = (shouldDoVerticalFirst ? verticalPart + horizontalPart : horizontalPart + verticalPart) + "A";
+            //Console.WriteLine(moves);
+            return (moves, this with { Position = endPoint });
         }
 
         private bool WillHitEmpty(Point start, Point end)
