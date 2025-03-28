ed751f18f7ad63a6b866d9adc205fd3000346f41
Sebastian Green <sgre@netlight.com>
2025-03-28 14:51:03 +0100

    some progress

--- 2024/AdventOfCode2024.Tests/Solutions/Day21.cs
+++ 2024/AdventOfCode2024.Tests/Solutions/Day21.cs
@@ -29,10 +29,10 @@ public class Day21 : ISolution
         //var input = Util.ReadRaw(SecondExample);
         var input = Util.ReadFile("day21");
 
-        var r = new RobotArrangement(input, [Directional, Directional, Directional, Numeric]);
-        var sum = r.CountScore();
+        var r = new RobotArrangement(input, 3);
+        var sum = r.CountOptimalScore();
         sum.Should().Be(177814);
-        
+        //sum.Should().Be(126384);
         // 184390L too high
         // 184058L too high
         // 184058L
@@ -41,12 +41,13 @@ public class Day21 : ISolution
     [Fact]
     public void Solution2()
     {
-        var input = Util.ReadRaw(Example);
-        //var input = Util.ReadFile("day20");
-
-        var r = new RobotArrangement(input, [Directional, Directional, Directional, Numeric]);
-        var sum = r.CountScore();
-        sum.Should().Be(1459L);
+        // I probably need to figure out "what does character X cost at Y levels above"
+        //var input = Util.ReadRaw(Example);
+        var input = Util.ReadFile("day21");
+        
+        var r = new RobotArrangement(input, 25);
+        var sum = r.CountOptimalScore();
+        sum.Should().Be(126384L);
     }
 
     /**
@@ -59,125 +60,52 @@ public class Day21 : ISolution
      * _ 0 A
      */
 
-    private class RobotArrangement(string[] input, PointMap<char>[] levels)
+    private class RobotArrangement(string[] input, int directionalLevels)
     {
         private string[] Targets { get; } = input;
+        
+        private static readonly Dictionary<(char Start, char End), string> OptimalMovesDirectional = OptimalParentMoves(Directional);
+        private static readonly Dictionary<(char Start, char End), List<string>> PossibleMovesNumeric = PossibleNumericParentMoves();
 
-        public long CountScore()
+
+        public long CountOptimalScore()
         {
             var score = 0L;
             foreach (var target in Targets)
             {
-                var shortestSequence = ShortestSequenceDijkstra(target);
+                var shortestSequence = ShortestSequenceOptimal(target);
                 var numeric = target.PlusMinusLongs().Single();
-                Console.WriteLine($"{target}: {shortestSequence.Length} * {numeric}");
-                score += shortestSequence.Length * numeric;
+                score += shortestSequence * numeric;
             }
 
             return score;
         }
 
-        private record MovePointer(string Moves, Pointer Pointer);
-
-        private record DijkstraState(int Level, string Moves, string Remainder, Pointer LowerLevelPointer)
+        private long ShortestSequenceOptimal(string target)
         {
-            public override string ToString() => $"{Level}, [{Moves} - {Remainder}]";
-        }
-        
-        private string ShortestSequenceDijkstra(string target)
-        {
-            var best = new DijkstraState(levels.Length - 1, "", target,
-                new Pointer(levels.Last().Single(kv => kv.Value == 'A').Key, levels.Last()));
-            var root = new Node<DijkstraState>(best, null);
-            var responses = root.Dijkstra(0, node =>
+            var totalMoves = "";
+            var pos = 'A';
+            foreach (var next in target)
             {
-                if (node.Value.Remainder == "")
-                {
-                    var nextLevelIndex = node.Value.Level - 1;
-                    return
-                    [
-                        new DijkstraState(nextLevelIndex, "", node.Value.Moves,
-                            new Pointer(levels[nextLevelIndex].Single(kv => kv.Value == 'A').Key,
-                                levels[nextLevelIndex]))
-                    ];
-                }
-
-                var primary = node.Value.LowerLevelPointer.NeededToPress(node.Value.Remainder[0], true);
-                var alternate = node.Value.LowerLevelPointer.NeededToPress(node.Value.Remainder[0], false);
-                return
-                [
-                    node.Value with
-                    {
-                        Moves = node.Value.Moves + primary.moves,
-                        Remainder = node.Value.Remainder[1..],
-                        LowerLevelPointer = primary.newPointer
-                    },
-                    node.Value with
-                    {
-                        Moves = node.Value.Moves + alternate.moves,
-                        Remainder = node.Value.Remainder[1..],
-                        LowerLevelPointer = alternate.newPointer
-                    }
-                ];
-            }, node =>
-            {
-                if (node.Value.Moves.Length == 0) return 0;
-                return node.Value.Moves.Length - node.Previous!.Value.Moves.Length;
-            }, node => node.Value is { Level: 1, Remainder.Length: 0 }, (node, dist) =>
-            {
-                if (node.Value.Level != 0) return false;
-
-                if (best.Level > 0)
-                {
-                    best = node.Value;
-                    return false;
-                }
-
-                var remainderDiff = best.Remainder.Length - node.Value.Remainder.Length;
-                var movesDiff = node.Value.Moves.Length - best.Moves.Length;
-                var botInSameSpot = node.Value.LowerLevelPointer.Position == best.LowerLevelPointer.Position;
-                if (remainderDiff > 0)
-                {
-                    best = node.Value;
-                    return false;
-                }
-
-                if (movesDiff == 0 && remainderDiff == 0 && botInSameSpot) return true;
-                if (node.Value.Moves.Length > 0 && best.Moves.Length > 0 && movesDiff > 0) return true;
-                return false;
-            });
-
-            return responses.First().EndNode.Value.Moves;
-        }
-
-        private string ShortestSequence(string target)
-        {
-            var moveOptions = new List<MovePointer> {new(target, new Pointer(levels.Last().Single(kv => kv.Value == 'A').Key, levels.Last()))};
-            for (var i = levels.Length - 1; i > 0; i--)
-            {
-                var newMoveOptions = new List<MovePointer>();
-                foreach (var moves in moveOptions)
-                {
-                    var lowerLevel = levels[i];
-                    var lowerPointer = moves.Pointer with { KeyPad = lowerLevel };
-                    var newMoves = "";
-                    foreach (var move in moves.Moves)
-                    {
-                        var primary = lowerPointer.NeededToPress(move, true);
-                        var alternate = lowerPointer.NeededToPress(move, false);
-                        var (upperMoves, newLowerPointer) =
-                            alternate.moves.Length < primary.moves.Length ? alternate : primary;
-                        newMoves += upperMoves;
-                        lowerPointer = newLowerPointer;
-                    }
-                    Console.WriteLine($"Level {i}: {moves} => {newMoves}");
-                    newMoveOptions.Add(new MovePointer(newMoves, lowerPointer));
-                }
-
-                moveOptions = newMoveOptions;
+                var possibleMoves = PossibleMovesNumeric[(pos, next)].Select(p => ParentMoves(p, directionalLevels - 1)).ToList();
+                totalMoves += possibleMoves.MinBy(p => p.Length);
+                pos = next;
             }
 
-            return moveOptions.MinBy(s => s.Moves.Length)!.Moves;
+            return totalMoves.Length;
+        }
+
+        private string ParentMoves(string moves, int levelsRemaining)
+        {
+            if (levelsRemaining == 0) return moves;
+            var movesNeeded = "";
+            var pos = 'A';
+            foreach (var move in moves)
+            {
+                movesNeeded += OptimalMovesDirectional[(pos, move)];
+                pos = move;
+            }
+            return ParentMoves(movesNeeded, levelsRemaining - 1);
         }
     }
 
@@ -185,31 +113,133 @@ public class Day21 : ISolution
 
     private static readonly PointMap<char> Numeric;
 
-    private record Pointer(Point Position, PointMap<char> KeyPad)
+    private static Dictionary<(char Start, char End), List<string>> PossibleNumericParentMoves()
     {
+        var moves = new Dictionary<(char Start, char End), List<string>>();
+        
+        foreach (var possibleMove in Numeric.Combinations())
+        {
+            var start = possibleMove[0];
+            var end = possibleMove[1];
+            if (start.Value == end.Value)
+            {
+                moves[(start.Value, end.Value)] = ["A"];
+            }
+            if (possibleMove.Any(m => m.Value == '.')) continue;
+
+            var working = new List<string>();
+            var possiblePaths = new Queue<string>();
+            possiblePaths.Enqueue("");
+            while (possiblePaths.TryDequeue(out var path))
+            {
+                if (path.Length > 3) continue;
+                var pos = start.Key;
+                var failed = false;
+                foreach (var dir in path)
+                {
+                    var move = dir switch
+                    {
+                        '>' => Point.Origin.Right,
+                        'v' => Point.Origin.Down,
+                        '<' => Point.Origin.Left,
+                        '^' => Point.Origin.Up,
+                        _ => throw new Exception("Not possible")
+                    };
+                    pos += move;
+                    if (!Numeric.ContainsKey(pos) || Numeric[pos] == '.') failed = true;
+                }
+                if (failed) continue;
+                if (pos == end.Key)
+                {
+                    working.Add(path + "A");
+                }
+                else
+                {
+                    possiblePaths.Enqueue(path + ">");
+                    possiblePaths.Enqueue(path + "v");
+                    possiblePaths.Enqueue(path + "<");
+                    possiblePaths.Enqueue(path + "^");
+                }
+            }
+
+            moves[(start.Value, end.Value)] = working;
+        }
+
+        return moves;
+    }
+    
+    /// <summary>
+    /// From any point S on the child map, to get to E, the moves M are needed.
+    /// For each move M, there is an optimal path P the parent can take from 'A' to get there, and press 'A' again.
+    /// This method finds the optimal P for all possible S and E
+    /// </summary>
+    private static Dictionary<(char Start, char End), string> OptimalParentMoves(PointMap<char> childMap)
+    {
+        var moves = new Dictionary<(char Start, char End), string>();
+        
+        foreach (var possibleMove in childMap.Combinations())
+        {
+            var start = possibleMove[0];
+            var end = possibleMove[1];
+            if (start.Value == end.Value)
+            {
+                moves[(start.Value, end.Value)] = "A";
+            }
+            if (possibleMove.Any(m => m.Value == '.')) continue;
+
+            var parentMoves = ParentPressesNeeded(end.Value.ToString(), new Robot(start.Key, childMap));
+
+            moves[(start.Value, end.Value)] = parentMoves;
+        }
+
+        return moves;
+    }
+
+    private static string ParentPressesNeeded(string childCharsToPress, Robot childStart)
+    {
+        var totalMoves = "";
+        var child = childStart;
+        foreach (var nextChildChar in childCharsToPress)
+        {
+            var childOptionOne = child.NeededToPress(nextChildChar, true);
+            var childOptionTwo = child.NeededToPress(nextChildChar, false);
+            var childMovesNeeded = childOptionOne.moves.Length <= childOptionTwo.moves.Length
+                ? childOptionOne
+                : childOptionTwo;
+            child = childMovesNeeded.newPointer;
+            totalMoves += childMovesNeeded.moves;
+        }
+
+        Console.WriteLine($"{childStart.Char()} making moves {childCharsToPress} requires {totalMoves}");
+        return totalMoves;
+    }
+
+    private record Robot(Point Position, PointMap<char> KeyPad)
+    {
+        public override string ToString() => $"{Position}({KeyPad[Position]} in " + (KeyPad == Directional ? "Directional": "Numerical") + ")";
+
         private readonly Point _void = KeyPad.Single(kv => kv.Value == '.').Key;
         
-        public (string moves, Pointer newPointer) NeededToPress(char end, bool prioritizeVertical)
+        public (string moves, Robot newPointer) NeededToPress(char end, bool prioritizeVertical)
         {
+            if (KeyPad[Position] == end) return ("A", this);
             var endPoint = KeyPad.Single(kv => kv.Value == end).Key;
             var move = endPoint - Position;
             var verticals = Math.Abs(move.Row);
             var horizontals = Math.Abs(move.Col);
             var verticalPart = string.Concat(Enumerable.Repeat(move.Row > 0 ? 'v' : '^', (int)verticals));
             var horizontalPart = string.Concat(Enumerable.Repeat(move.Col > 0 ? '>' : '<', (int)horizontals));
-            var hasToDoVerticalFirst = (endPoint.Col == _void.Col && Position.Row == _void.Row) || (endPoint.Row == _void.Row && Position.Col == _void.Col);
-            var hasToDoHorizontalFirst = (endPoint.Row == _void.Row && Position.Col == _void.Col) || (endPoint.Col == _void.Col && Position.Row == _void.Row);
+            // starting in void row and ending in void column
+            var hasToDoVerticalFirst = Position.Row == _void.Row && endPoint.Col == _void.Col;
+            // starting in void column and ending in void row
+            var hasToDoHorizontalFirst = Position.Col == _void.Col && endPoint.Row == _void.Row;
             var shouldDoVerticalFirst = hasToDoVerticalFirst ? true : hasToDoHorizontalFirst ? false : prioritizeVertical;
             var moves = (shouldDoVerticalFirst ? verticalPart + horizontalPart : horizontalPart + verticalPart) + "A";
             //Console.WriteLine(moves);
             return (moves, this with { Position = endPoint });
         }
 
-        private bool WillHitEmpty(Point start, Point end)
-        {
-            if (start.Col > 0 && end.Col > 0) return false;
-            return (start.Col == _void.Col && end.Row == _void.Row) || (start.Row == _void.Row && end.Col == _void.Col);
-        }
+        public char Char() => KeyPad[Position];
     }
 
     static Day21()
