27e13096959af979d628dd2e5ed9549908d86eaa
Sebastian Green <sebastianpetergreen@gmail.com>
2025-07-28 21:43:24 +0200

    2024 24 2 WIP

--- 2024/AdventOfCode2024.Tests/Solutions/Day24.cs
+++ 2024/AdventOfCode2024.Tests/Solutions/Day24.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
 // ReSharper disable InvalidXmlDocComment
 
 namespace AdventOfCode2024.Tests.Solutions;
@@ -68,25 +69,47 @@ public class Day24 : ISolution
                                         tnw OR pbm -> gnj
                                         """;
 
+    private const string P2Example = """
+                                     x00: 0
+                                     x01: 1
+                                     x02: 0
+                                     x03: 1
+                                     x04: 0
+                                     x05: 1
+                                     y00: 0
+                                     y01: 0
+                                     y02: 1
+                                     y03: 1
+                                     y04: 0
+                                     y05: 1
+
+                                     x00 AND y00 -> z05
+                                     x01 AND y01 -> z02
+                                     x02 AND y02 -> z01
+                                     x03 AND y03 -> z03
+                                     x04 AND y04 -> z04
+                                     x05 AND y05 -> z00
+                                     """;
+
     [Fact]
     public void Solution1()
     {
         //var input = Util.ReadRaw(LargeExample);
         var input = Util.ReadFile("day24");
 
-        var sum = new WireDiagram(input).GetOutput();
+        var sum = new WireDiagram(input).GetStartingOutput();
         sum.Should().Be(2024);
     }
 
     [Fact]
     public void Solution2()
     {
-        var input = Util.ReadRaw(Example);
-        //var input = Util.ReadFile("day24");
+        //var input = Util.ReadRaw(P2Example);
+        var input = Util.ReadFile("day24");
 
-        var sum = new WireDiagram(input).GetOutput();
+        var sum = new WireDiagram(input).WrongWiresSorted();
 
-        sum.Should().Be(12);
+        sum.Should().Be("z00,z01,z02,z05");
     }
 
     private class WireDiagram(string[] input)
@@ -98,15 +121,9 @@ public class Day24 : ISolution
                 line => line.Split(':')[1].Trim() == "1");
 
         private record GateLine(string Left, string Right, string Operand, string Output);
-        
-        public long GetOutput()
+
+        private IEnumerable<GateLine> GetGateLines()
         {
-            var outputs = new Dictionary<string, bool>();
-            foreach (var startingValue in StartingValues)
-            {
-                outputs[startingValue.Key] = startingValue.Value;
-            }
-            var toAdd = new Queue<GateLine>();
             foreach (var connection in input.SkipWhile(line => !string.IsNullOrWhiteSpace(line)).Skip(1))
             {
                 var split = connection.Split(' ');
@@ -114,16 +131,155 @@ public class Day24 : ISolution
                 var operand = split[1];
                 var i2 = split[2];
                 var o = split[4];
-                toAdd.Enqueue(new GateLine(i1, i2, operand, o));
+                yield return new GateLine(i1, i2, operand, o);
             }
+        }
+
+        private Dictionary<string, GateNode> GetGateNodes(IReadOnlyDictionary<string, bool> inputs)
+        {
+            var nodes = new Dictionary<string, GateNode>();
+            foreach (var i in inputs)
+            {
+                var node = new StaticGateNode
+                {
+                    Value = i.Value,
+                    Operand = '\0',
+                    Left = null!,
+                    Right = null!,
+                    Key = i.Key
+                };
+                node.Left = node;
+                node.Right = node;
+                nodes[i.Key] = node;
+            }
+
+            var unresolved = new Queue<GateLine>();
+            foreach (var gateLine in GetGateLines())
+            {
+                unresolved.Enqueue(gateLine);
+            }
+            
+            var unchangesCycles = 0;
+
+            while (unresolved.TryDequeue(out var connection))
+            {
+                if (unchangesCycles > unresolved.Count + 5) throw new Exception("Failed to solve system");
+                if (!nodes.TryGetValue(connection.Left, out var left) || !nodes.TryGetValue(connection.Right, out var right))
+                {
+                    unresolved.Enqueue(connection);
+                    unchangesCycles++;
+                    continue;
+                }
+                unchangesCycles = 0;
+                
+                nodes[connection.Output] = new GateNode
+                {
+                    Operand = connection.Operand switch
+                    {
+                        "AND" => '&',
+                        "OR" => '|',
+                        "XOR" => '^',
+                        _ => throw new ArgumentOutOfRangeException()
+                    },
+                    Left = left,
+                    Right = right,
+                    Key = connection.Output
+                };
+            }
+
+            return nodes;
+        }
+
+        private class GateNode
+        {
+            private bool? _outputCache;
+            private GateNode _left;
+            private GateNode _right;
+
+            public required string Key { get; init; }
+            public required char Operand { get; init; }
+            
+            public required GateNode Left
+            {
+                get => _left;
+                [MemberNotNull(nameof(_left))]
+                set
+                {
+                    _outputCache = false;
+                    _left = value;
+                }
+            }
+
+            public required GateNode Right
+            {
+                get => _right;
+                [MemberNotNull(nameof(_right))]
+                set
+                {
+                    _outputCache = false;
+                    _right = value;
+                }
+            }
+
+            public virtual bool Output()
+            {
+                return _outputCache ??= Operand switch
+                {
+                    '^' => Left.Output() != Right.Output(),
+                    '|' => Left.Output() || Right.Output(),
+                    '&' => Left.Output() && Right.Output(),
+                    _ => throw new ArgumentOutOfRangeException(nameof(Operand), Operand, null)
+                };
+            }
+
+            public override string ToString() => $"[{Key}]( {Left} {Operand} {Right} )";
+        }
+
+        private class StaticGateNode : GateNode
+        {
+            public required bool Value { get; init; }
+            public override bool Output() => Value;
+            
+            public override string ToString() => $"[{Key}]( " + (Value ? "1" : "0") + " )";
+        }
+
+        public long GetStartingOutput() => GetOutput(StartingValues);
+        
+        private long GetOutput(IReadOnlyDictionary<string, bool> inputs, params Swap[] swaps)
+        {
+            var outputs = new Dictionary<string, bool>();
+            foreach (var startingValue in inputs)
+            {
+                outputs[startingValue.Key] = startingValue.Value;
+            }
+            var toAdd = new Queue<GateLine>();
+            foreach (var gateLine in GetGateLines())
+            {
+                var swap = swaps.SingleOrDefault(swap => swap.Left == gateLine.Output || swap.Right == gateLine.Output);
+                if (swap is not null)
+                {
+                    var swappedOutput = swap.Left == gateLine.Output ? swap.Right : swap.Left;
+                    toAdd.Enqueue(gateLine with { Output = swappedOutput });
+                }
+                else
+                {
+                    toAdd.Enqueue(gateLine);
+                }
+            }
+
+            var unchangesCycles = 0;
 
             while (toAdd.TryDequeue(out var connection))
             {
+                if (unchangesCycles > toAdd.Count + 5) throw new Exception("Failed to solve system");
                 if (!outputs.TryGetValue(connection.Left, out var left) || !outputs.TryGetValue(connection.Right, out var right))
                 {
                     toAdd.Enqueue(connection);
+                    unchangesCycles++;
                     continue;
                 }
+
+                unchangesCycles = 0;
 
                 outputs[connection.Output] = connection.Operand switch
                 {
@@ -144,5 +300,95 @@ public class Day24 : ISolution
 
             return output;
         }
+
+        public string WrongWiresSorted()
+        {
+            return string.Join(',', FaultyWires().OrderBy(w => w));
+        }
+
+        private int InputLength => StartingValues.Keys.Select(k => int.Parse(k.Substring(1))).Max() + 1;
+
+        private IEnumerable<string> FaultyWires(params Swap[] swaps)
+        {
+            Dictionary<string, bool> GetBaseInput()
+            {
+                var output = new Dictionary<string, bool>();
+                for (var i = 0; i < InputLength; i++)
+                {
+                    output[$"x{i:00}"] = false;
+                    output[$"y{i:00}"] = false;
+                }
+
+                return output;
+            }
+            
+            for (var i = 1; i < InputLength; i++)
+            {
+                var prevLeft = $"x{i - 1:00}";
+                var prevRight = $"y{i - 1:00}";
+                var left = $"x{i:00}";
+                var right = $"y{i:00}";
+                var output = $"z{i:00}";
+                var carry = $"z{i + 1:00}";
+                var valid = true;
+                foreach (var variant in Cases)
+                {
+                    var inputs = GetBaseInput();
+                    inputs[left] = variant.Key.Left;
+                    inputs[right] = variant.Key.Right;
+                    if (variant.Key.Carry && i > 0)
+                    {
+                        inputs[prevLeft] = true;
+                        inputs[prevRight] = true;
+                    }
+                    var nodes = GetGateNodes(inputs);
+                    var result = nodes[output].Output();
+                    var carryResult = nodes[carry].Output();
+                    if (result != variant.Value.Output)
+                    {
+                        valid = false;
+                        break;
+                    }
+
+                    if (i > 0 && carryResult != variant.Value.Carry)
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (!valid) yield return output;
+            }
+        }
+
+        public record Swap(string Left, string Right);
+
+        private record Case(bool Left, bool Right, bool Carry);
+
+        private record Result(bool Output, bool Carry);
+
+        private Dictionary<Case, Result> Cases { get; } = new Case[]
+        {
+            new(false, false, false),
+            new(false, false, true),
+            new(false, true, false),
+            new(false, true, true),
+            new(true, false, false),
+            new(true, false, true),
+            new (true, true, false),
+            new(true, true, true)
+        }.ToDictionary(inp => inp,
+            inp =>
+            {
+                var total = new[] { inp.Left, inp.Right, inp.Carry }.Sum(b => b ? 1 : 0);
+                return total switch
+                {
+                    0 => new Result(false, false),
+                    1 => new Result(true, false),
+                    2 => new Result(false, true),
+                    3 => new Result(true, true),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            });
     }
 }
